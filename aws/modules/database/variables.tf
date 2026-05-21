50452fabbd498d49e30046285da075bfc012e68e
drdre4664 <drdre4664@gmail.com>
2026-05-21 17:02:41 -0400

    Create variables.tf

---
+++ aws/modules/database/variables.tf
@@ -0,0 +1,7 @@
+variable "project_name"      { type = string }
+variable "db_subnet_ids"     { type = list(string) }
+variable "db_sg_id"          { type = string }
+variable "db_name"           { type = string }
+variable "db_username"       { type = string }
+variable "db_password"       { type = string; sensitive = true }
+variable "db_instance_class" { type = string; default = "db.t3.micro" }
