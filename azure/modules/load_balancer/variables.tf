64b1d92d0b3dd34e46c2d7b66f505c2aa997369c
drdre4664 <drdre4664@gmail.com>
2026-05-21 17:12:59 -0400

    Create variables.tf

---
+++ azure/modules/load_balancer/variables.tf
@@ -0,0 +1,5 @@
+variable "project_name"        { type = string }
+variable "location"            { type = string }
+variable "resource_group_name" { type = string }
+variable "web_subnet_id"       { type = string }
+variable "app_subnet_id"       { type = string }
