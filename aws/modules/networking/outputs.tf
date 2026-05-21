aa4ebbc115eb044b5a6a78343c582dbb382a43f6
drdre4664 <drdre4664@gmail.com>
2026-05-21 17:00:26 -0400

    Add outputs for VPC and subnet IDs in networking module

---
+++ aws/modules/networking/outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id"             { value = aws_vpc.main.id }
+output "web_subnet_ids"     { value = aws_subnet.web[*].id }
+output "app_subnet_ids"     { value = aws_subnet.app[*].id }
+output "db_subnet_ids"      { value = aws_subnet.db[*].id }
+output "alb_public_sg_id"   { value = aws_security_group.alb_public.id }
+output "web_sg_id"          { value = aws_security_group.web.id }
+output "alb_internal_sg_id" { value = aws_security_group.alb_internal.id }
+output "app_sg_id"          { value = aws_security_group.app.id }
+output "db_sg_id"           { value = aws_security_group.db.id }
