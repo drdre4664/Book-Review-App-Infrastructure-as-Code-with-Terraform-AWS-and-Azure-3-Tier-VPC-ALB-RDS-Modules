75307a8a09b2f2ac18cdd81a32d8ab0f4de9dafe
drdre4664 <drdre4664@gmail.com>
2026-05-21 16:59:54 -0400

    Add variables for networking module

---
+++ aws/modules/networking/variables.tf
@@ -0,0 +1,6 @@
+variable "project_name"       { type = string }
+variable "vpc_cidr"           { type = string }
+variable "web_subnet_cidrs"   { type = list(string) }
+variable "app_subnet_cidrs"   { type = list(string) }
+variable "db_subnet_cidrs"    { type = list(string) }
+variable "availability_zones" { type = list(string) }
